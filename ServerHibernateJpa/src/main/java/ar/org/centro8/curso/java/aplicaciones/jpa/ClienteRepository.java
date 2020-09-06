a7290a3185f4f104bec1516315055bbe91c68c8c
fahaf <fahaf@192.168.0.10>
2020-09-06 19:49:44 -0300

    Ajuste Repository y Api Rest

--- ServerHibernateJpa/src/main/java/ar/org/centro8/curso/java/aplicaciones/jpa/ClienteRepository.java
+++ ServerHibernateJpa/src/main/java/ar/org/centro8/curso/java/aplicaciones/jpa/ClienteRepository.java
@@ -1,5 +1,58 @@
 package ar.org.centro8.curso.java.aplicaciones.jpa;
 
-public class ClienteRepository {
+import ar.org.centro8.curso.java.aplicaciones.entities.Cliente;
+import ar.org.centro8.curso.java.aplicaciones.interfaces.I_ClienteRepository;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class ClienteRepository implements I_ClienteRepository{
+
+    private EntityManagerFactory emf;
+
+    public ClienteRepository(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    
+       
+    @Override
+    public void save(Cliente cliente) {
+        if(cliente==null) return;
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(cliente);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void remove(Cliente cliente) {
+        if(cliente==null) return;
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        em.remove(cliente);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void update(Cliente cliente) {
+        if(cliente==null) return;
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(cliente);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public List<Cliente> getAll() {
+        List<Cliente> list=new ArrayList();
+        EntityManager em=emf.createEntityManager();
+        list=(List<Cliente>)em.createNamedQuery("Cliente.findAll").getResultList();
+        em.close();
+        return list;
+    }
     
 }
--- ServerHibernateJpa/src/main/java/ar/org/centro8/curso/java/aplicaciones/rest/ClienteServicio.java
+++ ServerHibernateJpa/src/main/java/ar/org/centro8/curso/java/aplicaciones/rest/ClienteServicio.java
@@ -1,80 +1,88 @@
 package ar.org.centro8.curso.java.aplicaciones.rest;
 import ar.org.centro8.curso.java.aplicaciones.entities.Cliente;
-import ar.org.centro8.curso.java.aplicaciones.enumerados.TipoDocumento;
+//import ar.org.centro8.curso.java.aplicaciones.enumerados.TipoDocumento;
+import ar.org.centro8.curso.java.aplicaciones.interfaces.I_ClienteRepository;
+import ar.org.centro8.curso.java.aplicaciones.jpa.ClienteRepository;
 import com.google.gson.Gson;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-//@Path("clientes/v1")
-//public class ClienteServicio {
+@Path("clientes/v1")
+public class ClienteServicio {
     
-//    private ClienteRepository cr=new ClienteRepository(Connector.getConnection());
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPAPU");
+    I_ClienteRepository cr = new ClienteRepository(emf);
     
-//    @GET
-//    public String info(){
-//        return "Servicio de clientes activo";
-//    }
+    @GET
+    public String info(){
+        return "Servicio de clientes activo";
+    }
     /*
     http://localhost:8080/Server/clientes/v1/alta?nombre=Juan&apellido=perez&tipoDocumento=DNI&numeroDocumento=90909091&direcion=medrano162&comentarios=null
     */
-//    @GET
-//    @Path("alta")
-//    public String alta(@QueryParam("nombre")String nombre,
-//            @QueryParam("apellido")String apellido,
-//            @QueryParam("tipoDocumento")String tipoDocumento,
-//            @QueryParam("numeroDocumento")String numeroDocumento,
-//            @QueryParam("direccion")String direccion,
-//            @QueryParam("comentarios")String comentarios
-//    ){
-//        Cliente cliente=new Cliente(
-//                nombre,
-//                apellido,
-//                TipoDocumento.valueOf(tipoDocumento),
-//                Integer.parseInt(numeroDocumento),
-//                direccion,
-//                comentarios
-//        );
-//        cr.save(cliente);
-//        return cliente.getId()+"";
-//    }
+    @GET
+    @Path("alta")
+    public String alta(@QueryParam("nombre")String nombre,
+            @QueryParam("apellido")String apellido,
+            @QueryParam("tipoDocumento")String tipoDocumento,
+            @QueryParam("numeroDocumento")String numeroDocumento,
+            @QueryParam("direccion")String direccion,
+            @QueryParam("comentarios")String comentarios
+    ){
+        Cliente cliente=new Cliente(
+                nombre,
+                apellido,
+                tipoDocumento,
+                numeroDocumento,
+                direccion,
+                comentarios
+        );        
+        cr.save(cliente);
+        return cliente.getId()+"";        
+    }
         
     
-//    @GET
-//    @Path("baja")
-//    public String baja(@QueryParam("id")String id){
-//        try {
-//            cr.remove(cr.getById(Integer.parseInt(id)));
-//            return "true";
-//        } catch (Exception e) {
-//            return "false";
-//        }
-//    }
-//    
-//        
-//    @GET
-//    @Path("all")
-//    public String getAll(){
-//        String text="";
-//        for(Cliente c:cr.getAll()) text+=c+"\n";
-//        return text;
-//    }
-//    
-//    @GET
-//    @Path("listJson")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public String listJson(){        
-//        return new Gson().toJson(cr.getAll());
-//    }
-//        
-//    
-//    @GET
-//    @Path("likeApellido")
-//    public String getLikeApellido(@QueryParam("apellido") String apellido){
-//        String text="";
-//        for(Cliente c: cr.getLikeApellido(apellido)) text+=c+"\n";
-//        return text;
-//    }
-//}
+    @GET
+    @Path("baja")
+    public String baja(@QueryParam("id")String id){
+        try {
+            cr.remove(cr.getById(Integer.parseInt(id)));
+            return "true";
+        } catch (Exception e) {
+            return "false";
+        }
+    }
+    
+        
+    @GET
+    @Path("all")
+    public String getAll(){
+        String text="";
+        for(Cliente c:cr.getAll()) text+=c+"\n";
+        return text;
+    }
+    
+    @GET
+    @Path("listJson")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String listJson(){        
+        return new Gson().toJson(cr.getAll());
+    }
+        
+    
+    @GET
+    @Path("likeApellido")
+    public String getLikeApellido(@QueryParam("apellido") String apellido){
+        String text="";
+        for(Cliente c: cr.getLikeApellido(apellido)) text+=c+"\n";
+        return text;
+    }
+    
+    
+   
+}
