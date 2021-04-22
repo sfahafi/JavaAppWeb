f1d932065114e9eea482317ca78a45f33d684b60
Sharbel <sfahafi18@gmail.com>
2021-04-21 22:28:51 -0300

    Cambio DB

---
+++ ServerHibernateJpa/src/main/java/ar/org/centro8/curso/java/aplicaciones/sql/PostgresqlHeroku.sql
@@ -0,0 +1,7 @@
+Host  ec2-52-23-45-36.compute-1.amazonaws.com
+Database  d492959tme0fq1
+User  ppavhtcdndqiwm
+Port  5432
+Password  edb15747a8050cff6c1af2481a36fad799b2107dfdcb704e07157ef0f366acfd
+URI  postgres://ppavhtcdndqiwm:edb15747a8050cff6c1af2481a36fad799b2107dfdcb704e07157ef0f366acfd@ec2-52-23-45-36.compute-1.amazonaws.com:5432/d492959tme0fq1
+Heroku CLI  heroku pg:psql postgresql-rugged-90875 --app api-rest-sf
